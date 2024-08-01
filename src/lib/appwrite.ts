bbdd9fd71e3a3a5411f450984dfadc98707d8023
Alex Kimoto <66242241+saarcoder@users.noreply.github.com>
2024-08-01 19:31:05 +0200

    Read data

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,5 +1,7 @@
-import { Client } from 'appwrite';
+import { Client, Databases } from 'appwrite';
 export const client = new Client();
+export const databases = new Databases(client);
+
 const projectID= import.meta.env.PUBLIC_APPWRITE_PROJECT_ID;
 const endpoint= import.meta.env.PUBLIC_APPWRITE_ENDPOINT;
 
